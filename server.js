bd392361f03c53062369994fc4596259187079dd
gppegoraro <gregory-dede@hotmail.com>
2025-04-06 00:57:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 // Permitir que o servidor receba dados em JSON
 app.use(express.json());
 
+// Rota principal para o caminho "/"
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando corretamente!');
+});
+
 // Webhook para receber mensagens
 app.post('/webhook/receber', (req, res) => {
     console.log('Mensagem recebida:', req.body);
